5a17b5089c0213640792953c56f1601847bab8c7
zhiyi.zhang <zhiyi.zhang@tcl.com>
2021-02-03 17:56:32 +0800

    [21][easy] add my java codes

---
+++ AlgoLeetcodeCn/java/src/com/leetcodecn/_21_easy/_21_My1.java
@@ -0,0 +1,104 @@
+package com.leetcodecn._21_easy;
+
+
+/**
+ * 21. 合并两个有序链表
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ * <p>
+ * 示例 1：
+ * 输入：l1 = [1,2,4], l2 = [1,3,4]
+ * 输出：[1,1,2,3,4,4]
+ * <p>
+ * 示例 2：
+ * 输入：l1 = [], l2 = []
+ * 输出：[]
+ * <p>
+ * 示例 3：
+ * 输入：l1 = [], l2 = [0]
+ * 输出：[0]
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1. 两个链表的节点数目范围是 [0, 50]
+ * 2. -100 <= Node.val <= 100
+ * 3. l1 和 l2 均按 非递减顺序 排列
+ * <p>
+ * <p>
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class _21_My1 {
+
+
+    /**
+     * 执行用时： 0 ms
+     * 内存消耗： 38 MB
+     *
+     * 双指针法
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        ListNode firstNode = null;
+        ListNode curIteratingNode = null;
+
+        if (l1.val <= l2.val) {
+            firstNode = l1;
+            l1 = l1.next;
+        } else {
+            firstNode = l2;
+            l2 = l2.next;
+        }
+        curIteratingNode = firstNode;
+
+        while (l1 != null && l2 != null) {
+            int val1 = l1.val;
+            int val2 = l2.val;
+
+            if (val1 <= val2) {
+                // 设置下一个节点来自 l1
+                curIteratingNode.next = l1;
+                curIteratingNode = l1;
+                l1 = l1.next;
+            } else {
+                // 设置下一个节点来自 l2
+                curIteratingNode.next = l2;
+                curIteratingNode = l2;
+                l2 = l2.next;
+            }
+        }
+
+        curIteratingNode.next = l1 != null ? l1 : l2;
+        return firstNode;
+    }
+}
+
+
+class ListNode {
+    public int val;
+    public ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ AlgoLeetcodeCn/java/src/com/leetcodecn/_21_easy/_21_My1Test.java
@@ -0,0 +1,93 @@
+package com.leetcodecn._21_easy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class _21_My1Test {
+
+    ListNode l1;
+    ListNode l2;
+    ListNode l3;
+    ListNode l4;
+
+    @BeforeEach
+    void init() {
+        ListNode n1 = new ListNode(1);
+        ListNode _n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode _n2 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+        ListNode _n3 = new ListNode(3);
+        ListNode n4 = new ListNode(4);
+        ListNode _n4 = new ListNode(4);
+
+        l1 = _n2;
+        _n2.next = n2;
+        n2.next = n4;
+//        setNodes(l1, new ListNode[]{n1, n2, n4});
+
+        l2 = _n1;
+        _n1.next = _n3;
+        _n3.next = _n4;
+
+        printNodesValues(l1, "l1 values=", 3);
+
+        printNodesValues(l2, "l2 values=", 3);
+    }
+
+    private void printNodesValues(ListNode startingNode, String tipsBeforePrint, int nodesCnt) {
+        ArrayList<Integer> nodesValues = new ArrayList<>(nodesCnt);
+        for (ListNode ptr = startingNode; ptr != null; ptr = ptr.next) {
+            nodesValues.add(ptr.val);
+        }
+        System.out.println(tipsBeforePrint);
+        for (Integer curNodeValue : nodesValues) {
+            System.out.print(curNodeValue);
+            System.out.print(' ');
+        }
+        System.out.println();
+    }
+
+    private List<Integer> getNodesValues(ListNode startingNode, int nodesCnt) {
+        ArrayList<Integer> nodesValues = new ArrayList<>(nodesCnt);
+        for (ListNode ptr = startingNode; ptr != null; ptr = ptr.next) {
+            nodesValues.add(ptr.val);
+        }
+        return nodesValues;
+    }
+
+    private void setNodes(ListNode firstNode, ListNode[] nodes) {
+        if (nodes == null || nodes.length == 0) return;
+        for (int i = 0; i <= nodes.length - 2; i++) {
+            nodes[i].next = nodes[i + 1];
+        }
+        firstNode = nodes[0];
+    }
+
+    @Test
+    void mergeTwoLists() {
+        ListNode node1 = new _21_My1().mergeTwoLists(null, null);
+        Assertions.assertEquals(null, node1);
+
+        ListNode _node1 = new _21_My1().mergeTwoLists(null, l2);
+        Assertions.assertEquals(l2, _node1);
+
+        ListNode __node1 = new _21_My1().mergeTwoLists(null, l1);
+        Assertions.assertEquals(l1, __node1);
+
+
+
+
+        ListNode node2 = new _21_My1().mergeTwoLists(l1, l2);
+        List<Integer> nodesValues = getNodesValues(node2, 6);
+        int[] expected = {1, 2, 2, 3, 4, 4};
+        Assertions.assertEquals(expected.length, nodesValues.size());
+        for (int i=0; i<nodesValues.size(); i++){
+            Assertions.assertEquals(expected[i], nodesValues.get(i).intValue());
+        }
+    }
+}
