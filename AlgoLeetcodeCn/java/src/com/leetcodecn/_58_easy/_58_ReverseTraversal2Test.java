706fe736445a3e05aca033a27b1ca6399041a4e2
zhiyi.zhang <zhiyi.zhang@tcl.com>
2021-03-01 16:27:25 +0800

    [58-easy] modify unit test

--- AlgoLeetcodeCn/java/src/com/leetcodecn/_58_easy/_58_ReverseTraversal2Test.java
+++ AlgoLeetcodeCn/java/src/com/leetcodecn/_58_easy/_58_ReverseTraversal2Test.java
@@ -8,31 +8,31 @@ class _58_ReverseTraversal2Test {
     @Test
     void lengthOfLastWord() {
         // 1. --全部是字母, 无空格
-        Assertions.assertEquals(3, _58_ReverseTraversal2_Best.lengthOfLastWord("aaa"));
+        Assertions.assertEquals(5, _58_ReverseTraversal2_Best.lengthOfLastWord("World"));
         // 2. --全部是空格, 无字母
         Assertions.assertEquals(0, _58_ReverseTraversal2_Best.lengthOfLastWord(" "));
 
         // 3. --同时包含空格和字母
 
         // 空格仅在最前边(1个空格)
-        Assertions.assertEquals(3, _58_ReverseTraversal2_Best.lengthOfLastWord(" aaa"));
+        Assertions.assertEquals(5, _58_ReverseTraversal2_Best.lengthOfLastWord(" World"));
         // 空格仅在最前边(多个空格)
-        Assertions.assertEquals(3, _58_ReverseTraversal2_Best.lengthOfLastWord("  aaa"));
+        Assertions.assertEquals(5, _58_ReverseTraversal2_Best.lengthOfLastWord("  World"));
         // 空格仅在中间(1个空格)
-        Assertions.assertEquals(5, _58_ReverseTraversal2_Best.lengthOfLastWord("Hello World"));
+        Assertions.assertEquals(5, _58_ReverseTraversal2_Best.lengthOfLastWord("Hi World"));
         // 空格仅在中间(多个空格)
-        Assertions.assertEquals(5, _58_ReverseTraversal2_Best.lengthOfLastWord("Hello  World"));
+        Assertions.assertEquals(5, _58_ReverseTraversal2_Best.lengthOfLastWord("Hi  World"));
         // 空格仅在最后面(1个空格)
-        Assertions.assertEquals(3, _58_ReverseTraversal2_Best.lengthOfLastWord("aaa "));
+        Assertions.assertEquals(5, _58_ReverseTraversal2_Best.lengthOfLastWord("World "));
         // 空格仅在最后面(多个空格)
-        Assertions.assertEquals(3, _58_ReverseTraversal2_Best.lengthOfLastWord("aaa   "));
+        Assertions.assertEquals(5, _58_ReverseTraversal2_Best.lengthOfLastWord("World   "));
         // 空格同时在最前面和中间
-        Assertions.assertEquals(5, _58_ReverseTraversal2_Best.lengthOfLastWord("  Hello  World"));
+        Assertions.assertEquals(5, _58_ReverseTraversal2_Best.lengthOfLastWord("  Hi  World"));
         // 空格同时在最前面和最后面
-        Assertions.assertEquals(3, _58_ReverseTraversal2_Best.lengthOfLastWord("  aaa   "));
+        Assertions.assertEquals(5, _58_ReverseTraversal2_Best.lengthOfLastWord("  World   "));
         // 空格同时在中间和最后面
-        Assertions.assertEquals(5, _58_ReverseTraversal2_Best.lengthOfLastWord("Hello  World  "));
+        Assertions.assertEquals(5, _58_ReverseTraversal2_Best.lengthOfLastWord("Hi  World  "));
         // 空格同时在最前面, 中间和最后面
-        Assertions.assertEquals(5, _58_ReverseTraversal2_Best.lengthOfLastWord("   Hello  World  "));
+        Assertions.assertEquals(5, _58_ReverseTraversal2_Best.lengthOfLastWord("   Hi  World  "));
     }
 }
