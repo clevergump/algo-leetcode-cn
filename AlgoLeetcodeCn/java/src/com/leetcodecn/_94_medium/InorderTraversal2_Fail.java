1417510e635de109ecff334729c4f2083d785289
clevergump <clevergump@qq.com>
2021-05-07 00:25:38 +0800

    [94-medium][fail] binaryTree traversal-iteration

---
+++ AlgoLeetcodeCn/java/src/com/leetcodecn/_94_medium/InorderTraversal2_Fail.java
@@ -0,0 +1,62 @@
+package com.leetcodecn._94_medium;
+
+import com.leetcodecn.common.helper.TreeNode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 迭代解法。 运行fail，test不通过，报错。
+ * 
+ * 需反复研究官方的迭代解法 {@link OfficialInorderTraversal2}
+ */
+public class InorderTraversal2_Fail {
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        if (root == null) return null;
+
+        List<Integer> ret = new LinkedList<>();
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        /** 当前正在遍历的节点 */
+        TreeNode current = root;
+        stack.push(current);
+        boolean shouldPush = true;
+
+
+        /** 只要栈不为空， 或者栈虽然空了但是当前正在遍历的节点仍有（左或右）子节点， 则循环就会继续运行下去 */
+        while (!stack.isEmpty() || containsChild(current)) {
+            if (shouldPush) {
+                pushAllLeftLIFO(stack, current);
+            }
+            current = stack.pop();
+            ret.add(current.val);
+            if (current.right != null) {
+                current = current.right;
+                stack.push(current);
+                shouldPush = true;
+            } else {
+                shouldPush = false;
+            }
+        }
+
+        return ret;
+    }
+
+    private static boolean containsChild(TreeNode node) {
+        if (node == null) return false;
+        return node.left != null || node.right != null;
+    }
+
+    /**
+     * 将给定的根节点的所有后代左子节点按后进先出的顺序放入给定的栈中
+     * @param stack 给定的栈
+     * @param root 给定的根节点
+     */
+    private static void pushAllLeftLIFO(LinkedList<TreeNode> stack, TreeNode root) {
+        if (root == null) return;
+        TreeNode currentLeft = root.left;
+        while (currentLeft != null) {
+            stack.push(currentLeft);
+            currentLeft = currentLeft.left;
+        }
+    }
+}
---
+++ AlgoLeetcodeCn/java/src/com/leetcodecn/_94_medium/OfficialInorderTraversal2.java
@@ -0,0 +1,30 @@
+package com.leetcodecn._94_medium;
+
+import com.leetcodecn.common.helper.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 官方的迭代解法。
+ * https://leetcode-cn.com/problems/binary-tree-inorder-traversal/solution/er-cha-shu-de-zhong-xu-bian-li-by-leetcode-solutio/
+ *
+ */
+public class OfficialInorderTraversal2 {
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        Deque<TreeNode> stk = new LinkedList<TreeNode>();
+        while (root != null || !stk.isEmpty()) {
+            while (root != null) {
+                stk.push(root);
+                root = root.left;
+            }
+            root = stk.pop();
+            res.add(root.val);
+            root = root.right;
+        }
+        return res;
+    }
+}
